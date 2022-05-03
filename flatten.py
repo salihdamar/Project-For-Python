165a9081d5b005d3a5caf9d3ca4b05d523a26ff6
Salih DAMAR <47279445+salihdamar@users.noreply.github.com>
2022-05-03 20:03:41 +0300

    Create flatten.py

---
+++ flatten.py
@@ -0,0 +1,11 @@
+l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+lnew = []
+def flatten(n):
+    for i in n:
+        if isinstance(i, list):
+            flatten(i)
+        else:
+            lnew.append(i)
+
+flatten(l)
+print(lnew)
